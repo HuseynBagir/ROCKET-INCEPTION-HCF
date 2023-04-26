f16d7e1bbb817a9b21d39d89d67c09d351fc566f
HuseynBagir <huseynb99@gmail.com>
2023-04-26 11:24:29 +0200

    added results with 10000

--- plotting_accuracies.py
+++ plotting_accuracies.py
@@ -16,24 +16,24 @@ diff = diff.merge(inc, how='left', left_on='dataset', right_on='dataset_name')
 
 plt.figure(figsize=(6,6))
 plt.plot([0,1],[0,1],c='black')
-plt.title('ROCKET-500+HCF-6+Inception-ppv+max\nvs\nROCKET-500+HCF-6+Inception-GAP', fontsize=12)
-plt.xlabel('ROCKET-500+HCF-6+Inception-ppv+max', fontsize=12)
-plt.ylabel('ROCKET-500+HCF-6+Inception-GAP', fontsize=12)
-plt.text(0.4,0.23,'ROCKET-500+HCF-6+Inception\n-ppv+max is better', fontsize=12)
-plt.text(0.05,0.63,'ROCKET-500+HCF-6+Inception\n-GAP is better',fontsize=12)
+plt.title('ROCKET-10000+HCF-6+Inception-ppv\nvs\nInception', fontsize=12)
+plt.xlabel('ROCKET-10000+HCF-6+Inception-ppv', fontsize=12)
+plt.ylabel('Inception', fontsize=12)
+plt.text(0.4,0.23,'ROCKET-10000+HCF-6+Inception\n-ppv is better', fontsize=12)
+plt.text(0.05,0.63,'Inception is better',fontsize=12)
 
 plt.xlim([0, 1])
 plt.ylim([0, 1])
 
-win = diff.loc[df.iloc[:,1]  > diff.iloc[:,6]]
-tie = diff.loc[df.iloc[:,1] == diff.iloc[:,6]]
-los = diff.loc[df.iloc[:,1]  < diff.iloc[:,6]]
-p_val = np.round(scipy.stats.wilcoxon(diff.iloc[:,6], diff.iloc[:,1])[1], 4)
+win = diff.loc[df.iloc[:,4]  > diff.iloc[:,9]]
+tie = diff.loc[df.iloc[:,4] == diff.iloc[:,9]]
+los = diff.loc[df.iloc[:,4]  < diff.iloc[:,9]]
+p_val = np.round(scipy.stats.wilcoxon(diff.iloc[:,9], diff.iloc[:,4])[1], 4)
 
-plt.scatter(win.iloc[:,1], win.iloc[:,6], s=120, c='blue', label=f'Win: {len(win)}')
-plt.scatter(tie.iloc[:,1], tie.iloc[:,6], s=120, c='green', label=f'Tie: {len(tie)}')
-plt.scatter(los.iloc[:,1], los.iloc[:,6], s=120, c='orange', label=f'Loss {len(los)}')
+plt.scatter(win.iloc[:,4], win.iloc[:,9], s=120, c='blue', label=f'Win: {len(win)}')
+plt.scatter(tie.iloc[:,4], tie.iloc[:,9], s=120, c='green', label=f'Tie: {len(tie)}')
+plt.scatter(los.iloc[:,4], los.iloc[:,9], s=120, c='orange', label=f'Loss {len(los)}')
 plt.plot([], [], ' ', label="p_val: " + str(p_val))
 
 plt.legend(fontsize=12)
-plt.savefig('ppv+max vs GAP.png')
+plt.savefig('ppv vs inception.png')
